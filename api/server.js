dc7efaab2e8f98f8ff40060c634fb866096a9796
Kotasaka <sakaguchikota@Kota-Sakaguchis-MacBook-Pro.local>
2023-05-27 02:06:35 +0900

    feat: PositiveとNegativeを判断して発言する

--- api/server.js
+++ api/server.js
@@ -9,13 +9,15 @@ const config = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 };
 
+const OPENAPI_KEY = process.env.OPENAPI_KEY
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
 app.post('/webhook', line.middleware(config), (req, res) => {
     console.log(req.body.events);
 
-    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
+    //ここのif分はdeveloper consoleの"接続確認"用
     if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
         res.send('Hello LINE BOT!(POST)');
         console.log('疎通確認用');
@@ -35,59 +37,96 @@ async function handleEvent(event) {
   }
 
   const message = event.message.text;
-  //"豚山行く？"以外の場合は反応しない
-  // if(event.message.text !== '豚山行く？') {
-  //   return client.replyMessage(event.replyToken, {
-  //     type: 'text',
-  //   });
-  // }
 
-  let replyText = '';
-  if (message.includes('豚山行く')){
-      replyText = 'ようこそ！豚山へ！'; //"行きたい！！"ってメッセージを送信
-      return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: replyText
-  });
+  // 豚山が含まれているか
+  if (!message.match(/豚山/)) {
+    return
   }
-  else if(message.includes('豚山行かへん？')){
-      replyText = '行ってやってもええで'; //"行きたい！！"ってメッセージを送信
-      return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: replyText
-  });
+
+  // 豚山をラーメンに置換
+  message = message.replaceAll(/豚山/g, 'ラーメン屋')
+
+  // はいかいいえで答えるようにメッセージを追加する
+  message = "「" + message + "」と発言している人は、ラーメン屋に行きたいと思っていますか。\n"
+              +   "「はい」か「いいえ」で答えてください"
+
+  const requestOptions = {
+    "method": "post",
+    "headers": {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer "+ OPENAPI_KEY
+    },
+    "payload": JSON.stringify({
+      "model": "gpt-3.5-turbo",
+      "messages": [
+          {"role": "user", "content": userMessage}
+      ]
+    })
   }
-  else if(message.includes('🐖🗻')){
-    replyText = '豚山行きたいです！'; //"行きたい！！"ってメッセージを送信
-    return client.replyMessage(event.replyToken, {
+  const response = fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+  const responseText = response.getContentText();
+  const json = JSON.parse(responseText);
+  const text = json['choices'][0]['message']['content'].trim();
+
+  let replyMessage;
+  if (text.match(/はい/)) {
+    replyMessage = "いいですねえ！他に行きたい人いますか？"
+  } else if (text.match(/いいえ/)) {
+    replyMessage = "何言ってるんですか！！"
+  }
+
+  return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: replyText
+    text: replyMessage
   });
-  }
-  else if(message.includes('豚山')){
-    var random = Math.random()
-    if(random <0.5){
-      replyText = 'そろそろ豚山行きたくなってきた？'; //メッセージを送信
-      return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: replyText
-      });
-    }
-    else if(random >= 0.5 && random < 0.9){
-      replyText = '豚山行きたいです！'; //メッセージを送信
-      return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: replyText
-      });
-    }
-    else if(random > 0.9){
-      replyText = '豚山はほどほどにしとき…'; //メッセージを送信
-      return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: replyText
-      });
-    }
-  }
+
+
+  // let replyText = '';
+  // if (message.includes('豚山行く')){
+  //     replyText = 'ようこそ！豚山へ！';
+  //     return client.replyMessage(event.replyToken, {
+  //     type: 'text',
+  //     text: replyText
+  // });
+  // }
+  // else if(message.includes('豚山行かへん？')){
+  //     replyText = '行ってやってもええで';
+  //     return client.replyMessage(event.replyToken, {
+  //     type: 'text',
+  //     text: replyText
+  // });
+  // }
+  // else if(message.includes('🐖🗻')){
+  //   replyText = '豚山行きたいです！';
+  //   return client.replyMessage(event.replyToken, {
+  //   type: 'text',
+  //   text: replyText
+  // });
+  // }
+  // else if(message.includes('豚山')){
+  //   var random = Math.random()
+  //   if(random <0.5){
+  //     replyText = 'そろそろ豚山行きたくなってきた？';
+  //     return client.replyMessage(event.replyToken, {
+  //     type: 'text',
+  //     text: replyText
+  //     });
+  //   }
+  //   else if(random >= 0.5 && random < 0.9){
+  //     replyText = '豚山行きたいです！';
+  //     return client.replyMessage(event.replyToken, {
+  //     type: 'text',
+  //     text: replyText
+  //     });
+  //   }
+  //   else if(random > 0.9){
+  //     replyText = '豚山はほどほどにしとき…';
+  //     return client.replyMessage(event.replyToken, {
+  //     type: 'text',
+  //     text: replyText
+  //     });
+  //   }
+  // }
 }
 
 app.listen(PORT);
